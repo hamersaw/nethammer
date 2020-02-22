255fe0f6de4ce3d2924840a30080ea4aba01987e
Daniel Rammer <hamersaw@protonmail.com>
2020-02-22 07:24:55 -0700

    added a few wifi/capture/* scripts containing pre-defined filters for specific traffic.

---
+++ wifi/capture/beacon.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+# {
+#   "description" : "use dumpcap to capture AP beacons",
+#   "options" : [
+#     {
+#       "name" : "net.interface",
+#       "description" : "network interface identifier",
+#       "flag" : "i",
+#       "required" : "true"
+#     },
+#     {
+#       "name" : "net.capture.file",
+#       "description" : "pcapng output file",
+#       "flag" : "w",
+#       "required" : "false",
+#       "default" : "beacon-YYYYmmddHHMMSS.pcapng"
+#     }
+#   ]
+# }
+
+usage="usage $(basename $0) -i <interface>"
+
+# parse arguments
+filename="$(pwd)/beacon-$(date +%Y%m%d%H%M%S).pcapng"
+while getopts 'hi:w:' opt; do
+    case ${opt} in
+        h)
+            echo "$usage"
+            exit 0
+            ;;
+        i) interface="$OPTARG" ;;
+        w) filename="$OPTARG" ;;
+        ?)
+            echo "$usage"
+            exit 1
+            ;;
+    esac
+done
+
+# ensure required arguments are set
+[ -z "$interface" ] && echo "$usage" && exit 1
+
+# capture traffic
+args="-i:$interface:-w:$filename:-f:wlan[0] == 0x80"
+
+OFS=$IFS; IFS=":"
+dumpcap $args
